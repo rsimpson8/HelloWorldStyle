c46beead5feb7815cf8fa770d477e409a0d43376
Arielle Monson <amonson3@huskers.unl.edu>
2020-09-03 21:13:51 +0000

    initial commit

---
+++ helloWorldStyle.c
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Author: Arielle Monson
+ * Date: 2020/09/02
+ * 
+ * A simple Hello World style program that prints my name
+ * 
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Arielle Monson\n");
+     
+     return 0;
+ }
